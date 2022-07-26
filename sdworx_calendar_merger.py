Tessares/sdworx-calendar

commit e6b627646d1cd4ad3d0084cab83e7c5949bde02a

cat: allow non ascii words
'Home office' is translated to 'Travail à domicile'. The 'à' was causing
issues here.

All we want at the end is to filter out info starting from a digit.

Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

sdworx_calendar_merger.py
@@ -272,12 +272,12 @@ def get_owner(value):
 
 def get_category(value):
 	cat = "off"
-	cats = re.findall(r'\(([a-z ]+)\)', value.lower())
+	cats = re.findall(r'\(([a-z].+)\)', value.lower())
 	if cats:
 		cat = cats[0]
 		if len(cats) > 1:
 			print("ERROR: found more than one cat: ", cats)
-	return re.sub(r'\s+', '', cat.title())
+	return cat.title()
 
 def print_all(owners):
 	for owner, types in sorted(owners.items()):

commit a69c47c0c3633fbe2dfcb794e9a4f9cd9cf89bf6

cat: stop after the first found ')'
Otherwise we have categories like 'Absent) (1D'.

Fixes: e6b6276 (cat: allow non ascii words)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

sdworx_calendar_merger.py
@@ -282,7 +282,7 @@ def get_owner(value):
 
 def get_category(value):
 	cat = "off"
-	cats = re.findall(r'\(([a-z].+)\)', value.lower())
+	cats = re.findall(r'\(([a-z][^\)]+)\)', value.lower())
 	if cats:
 		cat = cats[0]
 		if len(cats) > 1: